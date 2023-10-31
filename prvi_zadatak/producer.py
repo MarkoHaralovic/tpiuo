81d0af92dcf2f55b0963c15344804181cac87d71
Marko Haralović <team8.admin@fronted2022.onmicrosoft.com>
2023-10-31 21:02:52 +0100

    Modification of data flow

--- prvi_zadatak/producer.py
+++ prvi_zadatak/producer.py
@@ -1,10 +1,16 @@
 import asyncio
+import aiohttp
 import requests
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 EVENT_HUB_CONNECTION_STR = "Endpoint=sb://labos1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fenUEfj8GMD1r5u9A36xbh0WqGOoKfHVh+AEhBeuyBM="
-EVENT_HUB_NAME = "labos1_eventhub"
+EVENT_HUB_NAME = "eventhubfervjestina"
 REDDIT_BASE_URL = "https://www.reddit.com/r/dataengineering/top/.json?t=all&limit=10"
 HEADERS = {
     #limit could be added here, to reate a batch of 10 posts  
@@ -18,10 +24,17 @@ async def fetch_reddit_top_posts(after=None):
     if after:
         url += f"&after={after}"
     
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    data = response.json().get("data", {})
-    return data.get("children", []), data.get("after")
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url, headers=HEADERS) as response:
+                response.raise_for_status()
+                data = await response.json()
+                if data:
+                    print("data")
+                return data.get("data", {}).get("children", []), data.get("data", {}).get("after")
+        except aiohttp.ClientError as e:
+            logger.error(f"Request failed: {e}")
+            return [], None
 
 
 async def run():
@@ -36,20 +49,26 @@ async def run():
             after = None
             for _ in range(100):  #sending maximum of 1000 posts
                 top_posts, after = await fetch_reddit_top_posts(after)
+                print(f"After : {after}")
                 if not top_posts:
                     break
-                
+                print("before batch")
                 event_data_batch = await producer.create_batch()
-                
+                print("After batch")
                 for post in top_posts:
+                    print(f"Post : {post}")
                     post_data = post.get("data", {})
                     event_data_batch.add(EventData(str(post_data)))
 
                 await producer.send_batch(event_data_batch)
-                await asyncio.sleep(10)  ##creating loop to send batch of 10 posts every 10 seconds
+                logger.info(f"Sent batch of {len(top_posts)} posts to Event Hub.")
+                await asyncio.sleep(1)  ##creating loop to send batch of 10 posts every 10 seconds
 
 
-asyncio.run(run())
-
-while True:
-    pass
+if __name__ == "__main__":
+    try:
+        asyncio.run(run())
+    except KeyboardInterrupt:
+        logger.info("Interrupted by user. Exiting...")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
