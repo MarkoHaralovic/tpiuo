37513891803517d974575caf041e0b1e91ece1c9
Marko Haralović <team8.admin@fronted2022.onmicrosoft.com>
2023-11-12 17:22:49 +0100

    Modified consumer and producer, data is sent to event hub, read from there and stored to Data Lake

--- prvi_zadatak/consumer.py
+++ prvi_zadatak/consumer.py
@@ -1,60 +1,83 @@
 import asyncio
 from azure.eventhub.aio import EventHubConsumerClient
-from azure.storage.filedatalake import DataLakeServiceClient
+from azure.storage.filedatalake import (
+    DataLakeServiceClient,
+    DataLakeDirectoryClient,
+    FileSystemClient
+)
 import json
 from datetime import datetime
 import os   
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 EVENT_HUB_CONNECTION_STR = "Endpoint=sb://labos1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fenUEfj8GMD1r5u9A36xbh0WqGOoKfHVh+AEhBeuyBM="
-EVENT_HUB_NAME = "eventhubfervjestina" #name of the event hub, connection string connects to eventspace where this event hub is
+EVENT_HUB_NAME = "eventhubfervjestina"  #name of the event hub, connection string connects to eventspace where this event hub is
 CONSUMER_GROUP = "$Default"  # Using  the default consumer group    
-DATA_LAKE_ACCOUNT_NAME = 'tpioudatalake'
-DATA_LAKE_ACCOUNT_KEY = 'qdKc5IaalOQYOkKx7IcYrsYhNa1FrefWeC9g+Zg3tivqjf1HD4iEpXMpoIsBX1gnIB3bSE1/qlqg+AStpKwdBg=='
+STORAGE_ACCOUNT_NAME = 'tpioudatalake'
+STORAGE_ACCOUNT_KEY = 'qdKc5IaalOQYOkKx7IcYrsYhNa1FrefWeC9g+Zg3tivqjf1HD4iEpXMpoIsBX1gnIB3bSE1/qlqg+AStpKwdBg=='
 FILE_SYSTEM_NAME = 'consumerdatalake'
 
-data_lake_service_client = DataLakeServiceClient(
-    account_url=f"https://{DATA_LAKE_ACCOUNT_NAME}.dfs.core.windows.net",
-    credential=DATA_LAKE_ACCOUNT_KEY
-)
+
+service_client = DataLakeServiceClient(account_url=f"https://{STORAGE_ACCOUNT_NAME}.dfs.core.windows.net",
+                                       credential=STORAGE_ACCOUNT_KEY)
+
+file_system_client = service_client.get_file_system_client(file_system=FILE_SYSTEM_NAME)
+
+async def save_to_data_lake(event_data):
+    # Extract creation time from the event data
+    creation_time = datetime.fromtimestamp(event_data['created_utc'])
+    directory_path = f"{creation_time:%Y/%m/%d/%H/%M}"
+    file_name = f"{event_data['id']}.json"
+
+    data_to_write = json.dumps(event_data)
+
+    # Create directory hierarchy if it doesn't exist
+    directory_client = file_system_client.get_directory_client(directory_path)
+    directory_client.create_directory() 
+    
+    # Create a file and upload the content
+    file_client = directory_client.get_file_client(file_name)
+    file_client.create_file()
+
+    file_size = file_client.get_file_properties().size
+    
+    logging.info("Appending data")
+    file_client.append_data(data=data_to_write, offset=0, length=len(data_to_write)) 
+    logging.info("Flushing data")
+    file_client.flush_data(len(data_to_write))
 
 async def on_event(partition_context, event):
-    try:
-        data = json.loads(event.body_as_str(encoding="UTF-8"))
-        created_utc = datetime.utcfromtimestamp(data['created_utc'])
-        path = f"{created_utc.year}/{created_utc.month}/{created_utc.day}/{created_utc.hour}/{created_utc.minute}"
-        
-        file_system_client = data_lake_service_client.get_file_system_client(FILE_SYSTEM_NAME)
-        directory_client = file_system_client.get_directory_client(path)
-        await directory_client.create_directory()
-        
-        file_client = directory_client.get_file_client(f"{data['id']}.json")
-        await file_client.create_file()
-        await file_client.append_data(data=event.body_as_bytes(), offset=0, length=len(event.body_as_bytes()))
-        await file_client.flush_data(len(event.body_as_bytes()))
+    logger.info("Received event from partition {}".format(partition_context.partition_id))
+    #Extract the event data
+    event_data = event.body_as_json(encoding='UTF-8')
 
-        logger.info(f"Saved post {data['id']} to Data Lake at path: {path}")
+    try:
+        if isinstance(event_data, dict):
+            # Process a single post
+            await save_to_data_lake(event_data)
         
-    except Exception as e:
-        logger.error(f"Failed to process event: {e}")
+        await partition_context.update_checkpoint(event)
+        
+    except json.JSONDecodeError as e:
+        print(e)
+        logging.error(f"Failed to decode event data: {e}")
 
 async def main():
-    consumer_client = EventHubConsumerClient.from_connection_string(
+    client = EventHubConsumerClient.from_connection_string(
         conn_str=EVENT_HUB_CONNECTION_STR,
-        consumer_group="$Default",
-        eventhub_name=EVENT_HUB_NAME,
+        consumer_group=CONSUMER_GROUP,
+        eventhub_name=EVENT_HUB_NAME
     )
 
-    async with consumer_client:
-        await consumer_client.receive(on_event=on_event)
+    async with client:
+        await client.receive(
+            on_event=on_event,
+            starting_position="-1",  # "-1" is from the beginning of the partition.
+            )
 
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        logger.info("Interrupted by user. Exiting...")
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- prvi_zadatak/producer.py
+++ prvi_zadatak/producer.py
@@ -4,18 +4,19 @@ import requests
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 import logging
+import json
 
-
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 EVENT_HUB_CONNECTION_STR = "Endpoint=sb://labos1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fenUEfj8GMD1r5u9A36xbh0WqGOoKfHVh+AEhBeuyBM="
-EVENT_HUB_NAME = "eventhubfervjestina"
+EVENT_HUB_NAME = "eventhubfervjestina"  #name of the event hub, connection string connects to eventspace where this event hub is
+CONSUMER_GROUP = "$Default"  # Using  the default consumer group    
 REDDIT_BASE_URL = "https://www.reddit.com/r/dataengineering/top/.json?t=all&limit=10"
 HEADERS = {
-    #limit could be added here, to reate a batch of 10 posts  
+    # limit could be added here, to reate a batch of 10 posts
     "User-Agent": "Python/urllib"
-}  
+}
 # gotten from https://github.com/reddit-archive/reddit/wiki/API#rules
 
 
@@ -23,15 +24,18 @@ async def fetch_reddit_top_posts(after=None):
     url = REDDIT_BASE_URL
     if after:
         url += f"&after={after}"
-    
+
     try:
         async with httpx.AsyncClient() as client:
             response = await client.get(url, headers=HEADERS)
             response.raise_for_status()
             data = response.json()
             if data:
-                print("data")
-            return data.get("data", {}).get("children", []), data.get("data", {}).get("after")
+                # print("data")
+                pass
+            return data.get("data", {}).get("children", []), data.get("data", {}).get(
+                "after"
+            )
     except httpx.HTTPError as e:
         logger.error(f"Request failed: {e}")
         return [], None
@@ -43,34 +47,39 @@ async def run():
     # the event hub name.
     # code available at the link:https://learn.microsoft.com/en-us/azure/event-hubs/event-hubs-python-get-started-send?tabs=connection-string%2Croles-azure-portal
     producer = EventHubProducerClient.from_connection_string(
-        conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
+        conn_str=EVENT_HUB_CONNECTION_STR,
+        eventhub_name=EVENT_HUB_NAME
     )
     async with producer:
-            after = None
-            for _ in range(100):  #sending maximum of 1000 posts
-                top_posts, after = await fetch_reddit_top_posts(after)
-                print(f"After : {after}")
-                if not top_posts:
-                    break
-                print("before batch")
-                event_data_batch = await producer.create_batch()
-                print("After batch")
-                for post in top_posts:
-                    print(f"Post : {post}")
-                    post_data = post.get("data", {})
-                    event_data_batch.add(EventData(str(post_data)))
-
-                await producer.send_batch(event_data_batch)
-                logger.info(f"Sent batch of {len(top_posts)} posts to Event Hub.")
-                await asyncio.sleep(10)  ##creating loop to send batch of 10 posts every 10 seconds
+        after = None
+        for _ in range(100):  # sending maximum of 1000 posts
+            top_posts, _ = await fetch_reddit_top_posts(after)
+            if not top_posts:
+                break
+            event_data_batch = await producer.create_batch()
+            for post in top_posts:
+                post_data = post.get("data", {})
+                try:
+                    # Serialize the post data and add it to the batch
+                    event_data_batch.add(EventData(json.dumps(post_data)))
+                except ValueError as e:
+                    logger.info(f"Error adding post to batch: {e}")
+                                
+            await producer.send_batch(event_data_batch)
+            logger.info(f"Sent batch of {len(top_posts)} posts to Event Hub.")
+            await asyncio.sleep(
+                10
+            )  ##creating loop to send batch of 10 posts every 10 seconds
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(run())
     except KeyboardInterrupt:
+        print("keyboardInterrupt")
         logger.info("Interrupted by user. Exiting...")
     except Exception as e:
+        print(e)
         logger.error(f"An error occurred: {e}")
     while True:
         pass
