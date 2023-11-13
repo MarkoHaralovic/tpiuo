d09eae52baeeeaaf42f6abd16cbb07a8ded760ff
Marko Haralović <team8.admin@fronted2022.onmicrosoft.com>
2023-11-13 18:23:12 +0100

    Updated Azure Analytics Jobs

--- drugi_zadatak/new_producer.py
+++ drugi_zadatak/new_producer.py
@@ -2,7 +2,7 @@ import asyncio
 import requests
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
-
+import json
 EVENT_HUB_CONNECTION_STR = "Endpoint=sb://labos1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=6QvWYyyNRIDg8FCqPOxoufaNl++n5EiGt+AEhLHqXuY=;EntityPath=labos1eventhub"
 EVENT_HUB_NAME = "labos1eventhub" 
 REDDIT_BASE_URL = "https://www.reddit.com/r/dataengineering/top/.json?t=all&limit=10"
@@ -40,9 +40,9 @@ async def run():
             event_data_batch = await producer.create_batch()
             
             for post in top_posts:
-                print(post)
+                print(post["data"].keys())
                 post_data = post.get("data", {})
-                event_data_batch.add(EventData(str(post_data)))
+                event_data_batch.add(EventData(json.dumps(post_data)))
 
             await producer.send_batch(event_data_batch)
             await asyncio.sleep(10) 
